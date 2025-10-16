129b3c5e5bb1c2d2bc326468051133821c63ba9f
Ali <104919540+deliyim3434@users.noreply.github.com>
2025-10-16 21:27:51 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
+# config.py
 
+# Telegram'dan https://my.telegram.org adresinden alacağın bilgiler
+API_ID =  21194358 # Buraya kendi API_ID'ni yaz
+API_HASH = "9623f07eca023e4e3c561c966513a642"  # Buraya kendi API_HASH'ini yaz
 
-# my.telegram.org adresinden aldığınız bilgiler
-API_ID =  21194358 # Kendi API ID'nizi buraya yazın (Tırnaksız, sayı olarak)
-API_HASH = "9623f07eca023e4e3c561c966513a642"  # Kendi API Hash'inizi buraya yazın
+# @BotFather'dan alacağın bot token'ı
+BOT_TOKEN = "7651843103:AAGiChGdicvHQ9LOhV_Pk0hhqaF4fAWaEVw" # Buraya kendi BOT_TOKEN'ını yaz
 
-# @BotFather'dan aldığınız token
-BOT_TOKEN = "7651843103:AAGiChGdicvHQ9LOhV_Pk0hhqaF4fAWaEVw" # Kendi Bot Token'ınızı yazın
-
-# Pyrogram oturum dosyasının adı
-SESSION_NAME = "BAFDZnYArYSXbMisVe1qb0AXIGQfV8U7SuOztG9MDkgWf2v8viLp52mziCHMoiwRqIoZcuFR0y_r2Dr5Xt-z_N2AVV0oSA9tlww7ugGiCkA7ycVhWwdKqbjplxgeOWO802z77bqjObkRBQWSuim-19mzSyFeBXQvMsiHg0peP60GBg519pwgosUWM6n1jKS0Vamz-7MbHK2_oLkP48tvU1Io9QjriGvdffq71aQXWIUavp8SWeZcSYZQeUKM0jCeoaBwGbPYpmxproIKkfsRqgFknFlQ0HhPI1N9rmoK-UDIzCLpdKg-pdJLvVxBEd-inFs7oODVX37BfX9-NiW2ufU_u_d0TgAAAAHklkA8AA"
+# Userbot'un sesli sohbete katılabilmesi için gerekli.
+SESSION_STRING = "BAFDZnYArYSXbMisVe1qb0AXIGQfV8U7SuOztG9MDkgWf2v8viLp52mziCHMoiwRqIoZcuFR0y_r2Dr5Xt-z_N2AVV0oSA9tlww7ugGiCkA7ycVhWwdKqbjplxgeOWO802z77bqjObkRBQWSuim-19mzSyFeBXQvMsiHg0peP60GBg519pwgosUWM6n1jKS0Vamz-7MbHK2_oLkP48tvU1Io9QjriGvdffq71aQXWIUavp8SWeZcSYZQeUKM0jCeoaBwGbPYpmxproIKkfsRqgFknFlQ0HhPI1N9rmoK-UDIzCLpdKg-pdJLvVxBEd-inFs7oODVX37BfX9-NiW2ufU_u_d0TgAAAAHklkA8AA" # Buraya oluşturduğun SESSION_STRING'i YAZDIĞINDAN EMİN OL
